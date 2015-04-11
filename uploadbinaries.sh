06f34c211fa2135dce981b5596223d491cee47ce
Brandon Ording <bording@gmail.com>
2015-04-11 19:38:03 -0400

    Add OS name to make file name unique
    Despite separate packages in a bintray repo, filenames must be unique
    across the entire repository!

--- uploadbinaries.sh
+++ uploadbinaries.sh
@@ -7,6 +7,6 @@ zip -r binaries.zip libgit2
 BINTRAYUSER="bording"
 BINTRAYKEY="51d19b77bad09256980e7904f540bf012436a32f"
 
-curl -T binaries.zip -u$BINTRAYUSER:$BINTRAYKEY https://api.bintray.com/content/bording/generic/$TRAVIS_OS_NAME/$TRAVIS_BUILD_NUMBER/binaries-$TRAVIS_BUILD_NUMBER.zip?publish=1
+curl -T binaries.zip -u$BINTRAYUSER:$BINTRAYKEY https://api.bintray.com/content/bording/generic/$TRAVIS_OS_NAME/$TRAVIS_BUILD_NUMBER/binaries-$TRAVIS_OS_NAME-$TRAVIS_BUILD_NUMBER.zip?publish=1
 
 popd

a83f6172826cb849547c274225a239ef3b0b61e6
Brandon Ording <bording@gmail.com>
2015-05-02 17:06:24 -0400

    Skip upload if secure variables aren't available

--- uploadbinaries.sh
+++ uploadbinaries.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+if [ $TRAVIS_SECURE_ENV_VARS == "true" ]; then
+
 pushd nuget.package
 
 zip -r binaries.zip libgit2
@@ -9,3 +11,5 @@ BINTRAY_API_USER="nulltoken"
 curl -T binaries.zip -u$BINTRAY_API_USER:$BINTRAY_API_KEY https://api.bintray.com/content/libgit2/compiled-binaries/$TRAVIS_OS_NAME/$TRAVIS_BUILD_NUMBER/binaries-$TRAVIS_OS_NAME-$TRAVIS_BUILD_NUMBER.zip?publish=1
 
 popd
+
+fi

ee027d011ae980a9067231a7773da01f7d5b2dd1
nulltoken <emeric.fermas@gmail.com>
2015-10-04 17:35:25 +0200

    Display the download link of the produced binaries

--- uploadbinaries.sh
+++ uploadbinaries.sh
@@ -10,6 +10,8 @@ BINTRAY_API_USER="nulltoken"
 
 curl -T binaries.zip -u$BINTRAY_API_USER:$BINTRAY_API_KEY https://api.bintray.com/content/libgit2/compiled-binaries/$TRAVIS_OS_NAME/$TRAVIS_BUILD_NUMBER/binaries-$TRAVIS_OS_NAME-$TRAVIS_BUILD_NUMBER.zip?publish=1
 
+printf "\n\n-> https://dl.bintray.com/libgit2/compiled-binaries/binaries-%s-%s.zip\n\n" "$TRAVIS_OS_NAME" "$TRAVIS_BUILD_NUMBER"
+
 popd
 
 fi

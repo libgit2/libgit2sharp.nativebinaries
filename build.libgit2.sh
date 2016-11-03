66368932c7fb436758e04d2b9425aa3613ac0f14
Leonard Buskin <leonardbuskin@gmail.com>
2016-11-02 19:23:22 -0700

    Build libssh2 for Darwin (link w/ package for Linux)

--- build.libgit2.sh
+++ build.libgit2.sh
@@ -3,12 +3,44 @@
 LIBGIT2SHA=`cat ./nuget.package/libgit2/libgit2_hash.txt`
 SHORTSHA=${LIBGIT2SHA:0:7}
 
+if [ $OS == "Darwin" ]; then
+
+rm -rf libssh2/build
+mkdir libssh2/build
+pushd libssh2/build
+
+export _BINPATH=`pwd`
+
+cmake -DCMAKE_BUILD_TYPE:STRING=Release \
+      -DCMAKE_C_FLAGS=-fPIC \
+      -DBUILD_SHARED_LIBS=OFF \
+	  -DENABLE_ZLIB_COMPRESSION=ON \
+	  -DCMAKE_OSX_ARCHITECTURES="i386;x86_64" \
+      ..
+cmake --build .
+
+popd
+
+fi
+
 rm -rf libgit2/build
 mkdir libgit2/build
 pushd libgit2/build
 
 export _BINPATH=`pwd`
 
+if [ $OS == "Darwin" ]; then
+cmake -DCMAKE_BUILD_TYPE:STRING=Release \
+      -DBUILD_CLAR:BOOL=OFF \
+      -DUSE_SSH=ON \
+	  -DLIBSSH2_FOUND=ON \
+	  -DLIBSSH2_LIBRARIES=$TRAVIS_BUILD_DIR/libssh2/build/src/libssh2.a \
+	  -DLIBSSH2_INCLUDE_DIRS=$TRAVIS_BUILD_DIR/libssh2/include \
+      -DENABLE_TRACE=ON \
+      -DLIBGIT2_FILENAME=git2-ssh-$SHORTSHA \
+      -DCMAKE_OSX_ARCHITECTURES="i386;x86_64" \
+      ..
+else
 cmake -DCMAKE_BUILD_TYPE:STRING=Release \
       -DBUILD_CLAR:BOOL=OFF \
       -DUSE_SSH=ON \
@@ -16,6 +48,7 @@ cmake -DCMAKE_BUILD_TYPE:STRING=Release \
       -DLIBGIT2_FILENAME=git2-ssh-$SHORTSHA \
       -DCMAKE_OSX_ARCHITECTURES="i386;x86_64" \
       ..
+fi
 cmake --build .
 
 popd

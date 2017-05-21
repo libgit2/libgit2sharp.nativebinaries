468e84006060b653d784c28a4deb54fdff0ee236
Brandon Ording <bording@gmail.com>
2017-05-21 00:51:16 -0400

    Upload to single 'libgit2' bintray package

--- uploadbinaries.sh
+++ uploadbinaries.sh
@@ -8,7 +8,7 @@ zip -r binaries.zip runtimes
 
 BINTRAY_API_USER="nulltoken"
 
-curl -T binaries.zip -u$BINTRAY_API_USER:$BINTRAY_API_KEY https://api.bintray.com/content/libgit2/compiled-binaries/$RID/$TRAVIS_BUILD_NUMBER/binaries-$RID-$TRAVIS_BUILD_NUMBER.zip?publish=1
+curl -T binaries.zip -u$BINTRAY_API_USER:$BINTRAY_API_KEY https://api.bintray.com/content/libgit2/compiled-binaries/libgit2/$TRAVIS_BUILD_NUMBER/binaries-$RID-$TRAVIS_BUILD_NUMBER.zip?publish=1
 
 printf "\n\n-> https://dl.bintray.com/libgit2/compiled-binaries/binaries-%s-%s.zip\n\n" "$RID" "$TRAVIS_BUILD_NUMBER"
 

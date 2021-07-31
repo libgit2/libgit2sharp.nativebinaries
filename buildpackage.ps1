e008233edab4d6fa5ed08a1bea26a49a809f276d
Robert Dailey <rcdailey@gmail.com>
2021-07-30 19:55:26 -0500

    Move libgit2 PDB files to legacy symbol package
    Nuget supports two types of symbol packages: [legacy][1] and
    [snuget][2]. The latter happens to be the newer implementation. Legacy
    symbol packages only exist for compatibility.
    
    It should be noted that, unfortunately, snuget symbol packages do not
    support Windows PDBs (the kind you get from C++ code). They only support
    portable PDBs (managed). As such, this patch provides support for legacy
    symbol package format. Note the following information from the symbol
    package MSDN documentation page:
    
    > Native projects, such as C++ projects, produce Windows PDBs instead of
    > Portable PDBs. These are not supported by NuGet.org's symbol server.
    > Please use Legacy Symbol Packages instead.
    
    Fixes #111
    
    [1]: https://docs.microsoft.com/en-us/nuget/create-packages/symbol-packages
    [2]: https://docs.microsoft.com/en-us/nuget/create-packages/symbol-packages-snupkg

--- buildpackage.ps1
+++ buildpackage.ps1
@@ -8,4 +8,9 @@ $buildDate = (Get-Date).ToUniversalTime().ToString("yyyyMMddHHmmss")
 $versionSuffix = ""
 if ($pre.IsPresent) { $versionSuffix = "-pre$BuildDate" }
 
-.\nuget.exe Pack nuget.package\NativeBinaries.nuspec -Version $version$versionSuffix -NoPackageAnalysis
+# Create nuget package + legacy symbol pack
+# See: https://docs.microsoft.com/en-us/nuget/create-packages/symbol-packages
+.\nuget.exe pack nuget.package\NativeBinaries.nuspec `
+    -Symbols `
+    -Version $version$versionSuffix `
+    -NoPackageAnalysis

d5926f08a27f558098e9d6d7b6a7c5810fd624ab
David Christensen <drc@linux.ibm.com>
2024-10-31 16:20:33 -0400

    Add ppc64le support
    Signed-off-by: David Christensen <drc@linux.ibm.com>

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -5,10 +5,19 @@ echo "building for $RID"
 
 if [[ $RID =~ arm64 ]]; then
     arch="arm64"
+    cc_arch="arm64"
 elif [[ $RID =~ arm ]]; then
     arch="armhf"
+    cc_arch="armhf"
+elif [[ $RID == linux-ppc64le ]]; then
+    arch="ppc64le"
+    cc_arch="powerpc64le"
+elif [[ $RID == linux-musl-ppc64le ]]; then
+    arch="ppc64le"
+    cc_arch="ppc64le"
 else
     arch="amd64"
+    cc_arch="amd64"
 fi
 
 if [[ $RID == linux-musl* ]]; then
@@ -17,7 +26,7 @@ else
     dockerfile="Dockerfile.linux"
 fi
 
-docker buildx build -t $RID -f $dockerfile --build-arg ARCH=$arch .
+docker buildx build -t $RID -f $dockerfile --build-arg ARCH=$arch --build-arg CC_ARCH=$cc_arch .
 
 docker run -t -e RID=$RID --name=$RID $RID
 

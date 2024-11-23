16f0470339ffedc6d20f3b89f2101d33fe17a69e
Brandon Ording <bording@gmail.com>
2024-11-23 13:17:45 -0500

    Tweaks

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -5,19 +5,12 @@ echo "building for $RID"
 
 if [[ $RID =~ arm64 ]]; then
     arch="arm64"
-    cc_arch="arm64"
 elif [[ $RID =~ arm ]]; then
     arch="armhf"
-    cc_arch="armhf"
-elif [[ $RID == linux-ppc64le ]]; then
-    arch="ppc64le"
-    cc_arch="powerpc64le"
-elif [[ $RID == linux-musl-ppc64le ]]; then
-    arch="ppc64le"
-    cc_arch="ppc64le"
+elif [[ $RID =~ ppc64le ]]; then
+    arch="powerpc64le"
 else
     arch="amd64"
-    cc_arch="amd64"
 fi
 
 if [[ $RID == linux-musl* ]]; then
@@ -26,7 +19,7 @@ else
     dockerfile="Dockerfile.linux"
 fi
 
-docker buildx build -t $RID -f $dockerfile --build-arg ARCH=$arch --build-arg CC_ARCH=$cc_arch .
+docker buildx build -t $RID -f $dockerfile --build-arg ARCH=$arch .
 
 docker run -t -e RID=$RID --name=$RID $RID
 

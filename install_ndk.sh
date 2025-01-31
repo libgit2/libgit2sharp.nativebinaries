3eabb50a0214707eee6307eaf33cb39c82ddd322
Kibnet Philosoff <kibnet@hotmail.com>
2025-01-31 12:54:11 +0300

    fix

--- install_ndk.sh
+++ install_ndk.sh
@@ -1,34 +1,8 @@
 #!/bin/bash
 
-#####################
-# usage: ./thisscript.sh [repo_path]
-# the repo path used for locate the `local.properties` file,
-#  this script will append cmake.dir into it,
-#  if not specify, will use $GITHUB_WORKSPACE,
-#  if $GITHUB_WORKSPACE is empty, will use `/home/runner/work/PuppyGit/PuppyGit`, it is the actually value of $GITHUB_WORKSPACE
-#####################
-
-#echo "Updating package list..."
-#sudo apt update
-#
-#echo "Installing: curl cmake make tar libssl-dev maven unzip"
-#sudo apt install -y curl cmake make tar libssl-dev maven unzip
-
-
-
 export build_root=$HOME/libsbuild
 mkdir -p $build_root
 cd $build_root
-
-# export ndk_filename="android-ndk-r26d-linux"
-# echo "Downloading: ndk"
-# curl -L -o "${build_root}/$ndk_filename.zip" "https://dl.google.com/android/repository/${ndk_filename}.zip"
-# export ANDROID_NDK_ROOT=$build_root/android-ndk
-# mkdir -p $ANDROID_NDK_ROOT
-# echo "Extracting: ndk"
-# -q : only show msg when err
-# unzip -q "$ndk_filename.zip"
-# mv android-ndk-r26d/* $ANDROID_NDK_ROOT
 
 echo "Downloading: Android SDK"
 # ANDROID_HOME is android sdk root, is sdk root, not ndk root
@@ -37,13 +11,13 @@ export ANDROID_CMD_TOOLS=$TOOLS/android-cmdline-tools
 export ANDROID_HOME=$build_root/android-sdk
 export CMAKE_VERSION=3.31.1
 export NDK_VERSION=26.3.11579264
-# 创建目标目录以及依赖目录，虽然后面会删一下cmd tools目录，但这个创建依然是有意义的，意义在于创建依赖目录，所以即使后面会删除cmd tools目录，这条命令也依然应该保留
+
 mkdir -p $ANDROID_CMD_TOOLS
 mkdir -p $ANDROID_HOME
 curl -L -o cmdline-tools.zip https://dl.google.com/android/repository/commandlinetools-linux-11076708_latest.zip
-# 删一下，确保mv后目录结构不变
+
 rm -rf $ANDROID_CMD_TOOLS
-# 解压然后移动，确保先删后解压移动，以免当解压后的目录和移动的目标目录同名时误删
+
 unzip cmdline-tools.zip -d $TOOLS/
 mv $TOOLS/cmdline-tools $ANDROID_CMD_TOOLS
 export ANDROID_SDKMANAGER=$ANDROID_CMD_TOOLS/bin/sdkmanager
@@ -53,7 +27,7 @@ yes | $ANDROID_SDKMANAGER --install "cmake;$CMAKE_VERSION" --sdk_root=$ANDROID_H
 # $ANDROID_HOME/cmdline-tools/bin/sdkmanager --list --sdk_root=$ANDROID_HOME
 # cmake root dir
 export CMAKE_DIR=$ANDROID_HOME/cmake/$CMAKE_VERSION
-# cmake 可执行文件路径
+# cmake 
 export CMAKE_PATH=$CMAKE_DIR/bin/cmake
 
 echo "print cmake version"
@@ -65,7 +39,7 @@ yes | $ANDROID_SDKMANAGER --channel=0 --install "ndk;$NDK_VERSION" --sdk_root=$A
 
 echo "set sdk.dir to local.properties for gradle"
 REPO_PATH=${1:-$GITHUB_WORKSPACE}
-REPO_PATH=${REPO_PATH:-/home/runner/work/PuppyGit/PuppyGit}
+REPO_PATH=${REPO_PATH:-/home/runner/work/libgit2sharp.nativebinaries/libgit2sharp.nativebinaries}
 LOCAL_PROPERTIES_PATH=$REPO_PATH/local.properties
 echo -e "\nsdk.dir=$ANDROID_HOME" >> $LOCAL_PROPERTIES_PATH
 echo "local.properties at: $LOCAL_PROPERTIES_PATH"
